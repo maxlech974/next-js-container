fc8a659662b44722bf9fbbc8d4b28f463d6bef4d
Maxime LECHERE <maximelechere.dev@gmail.com>
2022-11-25 16:21:11 +0100

    test

--- next-app/src/pages/index.tsx
+++ next-app/src/pages/index.tsx
@@ -1,8 +1,15 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import { useEffect } from 'react'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+  useEffect( ()=> {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then(res => res.json())
+      .then(json => console.log(json))
+
+  }, [])
   return (
     <div className={styles.container}>
       <Head>
