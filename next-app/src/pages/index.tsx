74c1d7562b2d9d17ce7cc66d4591c2dba3d50c27
Maxime LECHERE <maximelechere.dev@gmail.com>
2022-11-25 15:35:43 +0100

    first commit

--- next-app/src/pages/index.tsx
+++ next-app/src/pages/index.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next app</title>
+        <title>Test</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
